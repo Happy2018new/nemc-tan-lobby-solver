10b2bb66bfc6362f852bfcfa4d4b6d524c9ade5e
Happy2018new <chenbo79800@163.com>
2025-09-24 18:20:01 +0800

    main.go: Minor changes

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/Happy2018new/nemc-tan-lobby-solver/bunker/auth"
+	"github.com/Happy2018new/nemc-tan-lobby-solver/core/nethernet"
+	"github.com/Happy2018new/nemc-tan-lobby-solver/core/raknet"
 	"github.com/Happy2018new/nemc-tan-lobby-solver/minecraft"
 	"github.com/Happy2018new/nemc-tan-lobby-solver/protocol/service"
 )
@@ -12,14 +14,14 @@ import (
 func main() {
 	if false {
 		client, err := auth.CreateClient(&auth.ClientOptions{
-			AuthServer: "http://127.0.0.1:80",
+			AuthServer: "AUTH SERVER ADDRESS",
 		})
 		if err != nil {
 			panic(err)
 		}
 		wrapper := auth.NewServerAccessWrapper(client, "YOUR FB TOKEN")
 
-		listenConfig, listener, roomID, err := service.Listen(wrapper, "献给机械の花束")
+		listenConfig, listener, roomID, err := service.Listen(wrapper, "ROOM NAME")
 		if err != nil {
 			panic(err)
 		}
@@ -27,7 +29,39 @@ func main() {
 
 		fmt.Printf("[SUCCESS] Create room: %d", roomID)
 		for {
-			fmt.Println(listener.Accept())
+			clientConn, err := listener.Accept()
+			if err != nil {
+				panic(err)
+			}
+
+			serverConn, err := raknet.Dial("BDS SERVER ADDRESS")
+			if err != nil {
+				panic(err)
+			}
+
+			go func() {
+				defer clientConn.Close()
+				defer serverConn.Close()
+				for {
+					pkData, err := clientConn.(*nethernet.Conn).ReadPacket()
+					if err != nil {
+						return
+					}
+					serverConn.Write(append([]byte{0xfe}, pkData...))
+				}
+			}()
+
+			go func() {
+				defer clientConn.Close()
+				defer serverConn.Close()
+				for {
+					pkData, err := serverConn.ReadPacket()
+					if err != nil {
+						return
+					}
+					clientConn.Write(pkData[1:])
+				}
+			}()
 		}
 	}
 
@@ -39,7 +73,7 @@ func main() {
 			panic(err)
 		}
 
-		wrapper := auth.NewClientAccessWrapper(client, "124674", "", "YOUR FB TOKEN")
+		wrapper := auth.NewClientAccessWrapper(client, "ROOM ID", "", "YOUR FB TOKEN")
 		netConn, tanLobbyLoginResp, err := service.Dial(wrapper)
 		if err != nil {
 			panic(err)
